aa9f50a84e8045b11ae22e198749c327ffe3d1a1
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-05-03 22:30:26 +0300

    second-task-done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ingredients.forEach(ingredient => {
+  const element = document.createElement('li')
+  console.log(element)
+  element.textContent = ingredient
+  element.className = "item"
+  document.querySelector("ul#ingredients").append(element)
+})
