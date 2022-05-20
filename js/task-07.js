56503a8167db002eab623f97bb0769519607aeff
serhiiarkhypiuk <airborn.gnus@gmail.com>
2022-05-20 19:36:02 +0300

    initial commit

--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const slider = document.querySelector("input#font-size-control")
-const text = document.querySelector("span#text")
-
-const setFontSize = () => {
-    text.style.fontSize = slider.value +'px'
-}
-
-slider.addEventListener("input", setFontSize)
-slider.dispatchEvent(new Event('input'))
